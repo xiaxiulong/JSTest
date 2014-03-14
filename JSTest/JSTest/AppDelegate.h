8ba23d98c45a8ac53efbadec9a6231a75340e793
xiaxl <xiaxiulong888@163.com>
2014-03-14 11:52:41 +0800

    add jstest project

---
+++ JSTest/JSTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSTest
+//
+//  Created by xiaxl on 14-3-14.
+//  Copyright (c) 2014年 xiaxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
